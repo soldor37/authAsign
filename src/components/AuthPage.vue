610bd8e4d9ed56f27d5dc5edf567bcebd400a56f
soldor37 <seventhpirate@gmail.com>
2020-06-27 10:28:04 +0300

    init

---
+++ src/components/AuthPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="auth">
+    <h1>hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import AuthPage from './components/AuthPage.vue'
 
 Vue.config.productionTip = false
 
+const routes = [
+  { 
+    path: '/auth',
+    component: AuthPage,
+    name: 'auth',
+    // meta: { 
+    //   requiresAuth: true,
+    //   is_admin: true
+    // } 
+  },
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
