8ad57b3fb0e772445a1eebd9861e98327c61b3e1
soldor37 <seventhpirate@gmail.com>
2020-06-30 11:50:59 +0300

    margin td on mobile

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,45 +1,45 @@
 <template>
-<div class="page">
-  <div class="users">
-    <div class="usersTable__toolbar">
-          <div class="usersTable__toolbar__title">Users</div>
-          <input
-            type="text"
-            class="userTable__toolbar__search"
-            v-model="searchInput"
-            @keyup="searchUser()"
-            placeholder="Search for username.."
-          />
+  <div class="page">
+    <div class="users">
+      <div class="usersTable__toolbar">
+        <div class="usersTable__toolbar__title">Users</div>
+        <input
+          type="text"
+          class="userTable__toolbar__search"
+          v-model="searchInput"
+          @keyup="searchUser()"
+          placeholder="Search for username.."
+        />
       </div>
       <div class="usersTable__toolbar__sortBtn" @click="sortTable(0)">Sort by ID</div>
-    <table class="usersTable" id="usersTable">
-      <thead>
-        <tr class="usersTable__headers">
-          <th @click="sortTable(0)">
-            ID
-            <i class="arrow"></i>
-          </th>
-          <th>Username</th>
-          <th>First name</th>
-          <th>Last name</th>
-          <th>Is active</th>
-          <th>Last login</th>
-          <th>Is superuser</th>
-        </tr>
-      </thead>
-      <tbody class="usersTable__data">
-        <tr v-for="user in users" v-bind:key="user.id">
-          <td>{{user.id}}</td>
-          <td>{{user.username}}</td>
-          <td>{{user.first_name}}</td>
-          <td>{{user.last_name}}</td>
-          <td>{{user.is_active}}</td>
-          <td>{{user.last_login}}</td>
-          <td>{{user.is_superuser}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+      <table class="usersTable" id="usersTable">
+        <thead>
+          <tr class="usersTable__headers">
+            <th @click="sortTable(0)">
+              ID
+              <i class="arrow"></i>
+            </th>
+            <th>Username</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Is active</th>
+            <th>Last login</th>
+            <th>Is superuser</th>
+          </tr>
+        </thead>
+        <tbody class="usersTable__data">
+          <tr v-for="user in users" v-bind:key="user.id">
+            <td>{{user.id}}</td>
+            <td>{{user.username}}</td>
+            <td>{{user.first_name}}</td>
+            <td>{{user.last_name}}</td>
+            <td>{{user.is_active}}</td>
+            <td>{{user.last_login}}</td>
+            <td>{{user.is_superuser}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -50,7 +50,7 @@ export default {
   data() {
     return {
       users: [],
-      searchInput:'',
+      searchInput: ""
       // testUsers: [
       //   {
       //     id: 0,
@@ -171,14 +171,13 @@ export default {
     },
     searchUser() {
       let app = this;
-      let filter
-      let table
+      let filter;
+      let table;
       let tr, td, i;
       filter = app.searchInput.toUpperCase();
       table = document.getElementById("usersTable");
       tr = table.getElementsByTagName("tr");
-      //console.log(filter, table, tr)
-      // Loop through all table rows, and hide those who don't match the search query
+
       for (i = 1; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[1];
         if (td) {
@@ -195,7 +194,7 @@ export default {
       axios
         .get(`http://emphasoft-test-assignment.herokuapp.com/api/v1/users/`, {
           headers: {
-            Authorization: "Token " + localStorage.token,
+            Authorization: "Token " + localStorage.token
           }
         })
         .then(response => {
@@ -207,7 +206,7 @@ export default {
           console.log("-----error-------");
           console.log(error);
         });
-    },
+    }
   }
 };
 </script>
@@ -217,7 +216,7 @@ export default {
 .usersTable__data tr:hover {
   background-color: #f5f5f5;
 }
-.usersTable__toolbar{
+.usersTable__toolbar {
   display: flex;
   flex-flow: row nowrap;
   width: 100%;
@@ -234,7 +233,7 @@ export default {
   flex-flow: column wrap; */
   margin: 10px;
 }
-.users{
+.users {
   margin: auto;
   overflow: auto;
   min-width: 803px;
@@ -264,14 +263,14 @@ td {
   transform: rotate(-135deg);
   -webkit-transform: rotate(-135deg);
 }
-.userTable__toolbar__search{
-  width: 100%; 
-  font-size: 16px; 
-  padding: 12px 20px 12px 40px; 
-  border: 1px solid #ddd; 
-  margin-bottom: 12px; 
+.userTable__toolbar__search {
+  width: 100%;
+  font-size: 16px;
+  padding: 12px 20px 12px 40px;
+  border: 1px solid #ddd;
+  margin-bottom: 12px;
 }
-.usersTable__toolbar__sortBtn{
+.usersTable__toolbar__sortBtn {
   display: none;
   width: 80px;
   height: 30px;
@@ -290,50 +289,72 @@ td {
   transition: all 0.218s ease 0s;
 }
 @media only screen and (max-width: 2080px) {
-td {
- max-width: 100px;
- word-wrap:break-word;
-}
+  td {
+    max-width: 100px;
+    word-wrap: break-word;
+  }
 }
 @media only screen and (max-width: 850px) {
-  .users{
+  .users {
     min-width: 305px;
   }
-  .usersTable__toolbar__sortBtn{
+  .usersTable__toolbar__sortBtn {
     display: block;
   }
-  .userTable__toolbar__search{
+  .userTable__toolbar__search {
     padding: 12px 20px 12px 10px;
   }
-  table, thead, tbody, th, td, tr { 
-		display: block; 
-	}
-  thead tr { 
-		position: absolute;
-		top: -9999px;
-		left: -9999px;
-	}
-  tr { border: 1px solid #ccc; }
-  td { 
-		border: none;
-		border-bottom: 1px solid #eee; 
-		position: relative;
-		padding-left: 50%; 
-	}
-  td:before { 
-		position: absolute;
-		top: 6px;
-		left: 6px;
-		width: 45%; 
-		padding-right: 10px; 
-		white-space: nowrap;
-	}
-  td:nth-of-type(1):before { content: "ID"; }
-	td:nth-of-type(2):before { content: "Username"; }
-	td:nth-of-type(3):before { content: "First name"; }
-	td:nth-of-type(4):before { content: "Last name"; }
-	td:nth-of-type(5):before { content: "Is active"; }
-	td:nth-of-type(6):before { content: "Last login"; }
-	td:nth-of-type(7):before { content: "Is superuser"; }
+  table,
+  thead,
+  tbody,
+  th,
+  td,
+  tr {
+    display: block;
+  }
+  thead tr {
+    position: absolute;
+    top: -9999px;
+    left: -9999px;
+  }
+  tr {
+    border: 1px solid #ccc;
+  }
+  td {
+    border: none;
+    border-bottom: 1px solid #eee;
+    position: relative;
+    padding-left: 50%;
+    margin: auto;
+  }
+  td:before {
+    position: absolute;
+    top: 6px;
+    left: 6px;
+    width: 45%;
+    padding-right: 10px;
+    white-space: nowrap;
+  }
+  td:nth-of-type(1):before {
+    content: "ID";
+  }
+  td:nth-of-type(2):before {
+    content: "Username";
+  }
+  td:nth-of-type(3):before {
+    content: "First name";
+  }
+  td:nth-of-type(4):before {
+    content: "Last name";
+  }
+  td:nth-of-type(5):before {
+    content: "Is active";
+  }
+  td:nth-of-type(6):before {
+    content: "Last login";
+  }
+  td:nth-of-type(7):before {
+    content: "Is superuser";
+  }
 }
 </style>
